d05ce3564bf8eb8f327e0aea31c30ba32e2d0d5c
Milan Miletic <milan007miletic@gmail.com>
2021-11-29 19:58:19 +0000

    add LSTM and LinearSVC (no tuning yet)

--- IMDB-Sentiment-Analysis.py
+++ IMDB-Sentiment-Analysis.py
@@ -11,17 +11,25 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import metrics
 from sklearn.naive_bayes import MultinomialNB
 import matplotlib.pyplot as plt
+from sklearn.svm import LinearSVC
+from xgboost import XGBClassifier
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, Bidirectional, LSTM, Dense, Dropout, Input
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers import Adam
 
-tsv_data = pd.read_csv('movie_reviews2.tsv', sep='\t')
+tsv_data = pd.read_csv('movie_reviews3.tsv', sep='\t')
 tsv_data.columns = ['Ratings','Reviews']
-tsv_data['binaryRatings'] = np.where(tsv_data['Ratings'] <=5, -1, 1)
+tsv_data['binaryRatings'] = np.where(tsv_data['Ratings'] <= 5, -1, 1)
 
 print("Binary Ratings")
 print(tsv_data.head())
 
-tsv_data.to_csv('labelledData2.tsv', encoding='utf-8', index=False)
+tsv_data.to_csv('labelledData3.tsv', encoding='utf-8', index=False)
 
-tsv_data = tsv_data.drop(tsv_data.loc[tsv_data['binaryRatings'] == 1].sample(frac=0.5).index)
+tsv_data = tsv_data.drop(tsv_data.loc[tsv_data['binaryRatings'] == 1].sample(frac=0.56).index)
 
 #Visualizing Data
 figure = plt.figure(figsize=(5,5))
@@ -87,7 +95,7 @@ tsv_data["Reviews"] = tsv_data["Reviews"].apply(lambda text: lemmatizeWords(text
 print("Lemmatize")
 print(tsv_data.head())
 
-tsv_data.to_csv('PreprocessedData2.tsv', encoding='utf-8', index=False)
+tsv_data.to_csv('PreprocessedData3.tsv', encoding='utf-8', index=False)
 
 train, test = train_test_split(tsv_data, test_size=0.2, random_state=42, shuffle=True)
 tf_idf = TfidfVectorizer()
@@ -112,4 +120,92 @@ print(metrics.classification_report(train['binaryRatings'], trainRatingPredictio
 print("Testing Confusion Matrix")
 print(metrics.classification_report(test['binaryRatings'], testRatingPrediction))
 
-#Logistic Regression
+# TODO: Logistic Regression
+
+
+# Linear SVC
+lin_svc = LinearSVC()
+lin_svc.fit(Xtrain_tf, train['binaryRatings'])
+pred_svc = lin_svc.predict(Xtest_tf)
+
+print(metrics.classification_report(test['binaryRatings'], pred_svc))
+
+# Just playing around with another model
+# XG-Boost
+clf3 = XGBClassifier()
+clf3.fit(Xtrain_tf, train['binaryRatings'])
+preds3 = clf3.predict(Xtest_tf)
+
+print(metrics.classification_report(test['binaryRatings'], preds3))
+
+####################################################################
+####################################################################
+####################################################################
+
+# Neural Networks -- LSTM
+# Replace -1 with 0 labels for negative reviews
+train_nn = train.replace({'binaryRatings': {-1: 0}})
+test_nn = test.replace({'binaryRatings': {-1: 0}})
+
+
+# Split train and test data into reviews and labels
+X_train = train['Reviews'].values
+y_train = train['binaryRatings'].values
+X_test = test['Reviews'].values
+y_test = test['binaryRatings'].values
+
+# Fit Keras tokenizer on train reviews
+vocab_size = 10000
+tokenizer = Tokenizer(num_words=vocab_size)
+tokenizer.fit_on_texts(X_train)
+
+# Encode train and test reviews
+X_train_seq = tokenizer.texts_to_sequences(X_train)
+X_test_seq = tokenizer.texts_to_sequences(X_test)
+
+# Some statistics about the reviews
+l = np.array([len(i) for i in X_train_seq])
+print('minimum number of words in a review: {}'.format(l.min()))
+print('median number of words in a review: {}'.format(np.median(l)))
+print('average number of words in a review: {}'.format(l.mean()))
+print('maximum number of words in a review: {}'.format(l.max()))
+
+# Limit review length to max_len tokens
+max_len = 385
+
+# Use padding so that all sequences are of same length
+X_train_seq_padded = pad_sequences(X_train_seq, maxlen=max_len)
+X_test_seq_padded = pad_sequences(X_test_seq, maxlen=max_len)
+
+
+# Defining the model
+inputs = Input(shape=(max_len,))
+x = Embedding(vocab_size, 338)(inputs)
+x = LSTM(units=8)(x)
+x = Dense(111, activation="relu")(x)
+x = Dropout(0.5)(x)
+outputs = Dense(1, activation="sigmoid")(x)
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+optimizer = Adam(learning_rate=0.0009)
+model.compile(optimizer=optimizer,
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+history = model.fit(x=X_train_seq_padded,
+                    y=y_train,
+                    batch_size=56,
+                    epochs=35,
+                    validation_data=(X_test_seq_padded, y_test),
+                    callbacks=[EarlyStopping(monitor='val_accuracy', patience=3)])
+
+# Predictions
+preds_test = model.predict(X_test_seq_padded)
+preds_test = np.array([1 if pred > 0.5 else 0 for pred in preds_test.flatten()])
+
+preds_train = model.predict(X_train_seq_padded)
+preds_train = np.array([1 if pred > 0.5 else 0 for pred in preds_train.flatten()])
+
+print(metrics.classification_report(y_test, preds_test))
+print(metrics.classification_report(y_train, preds_train))
