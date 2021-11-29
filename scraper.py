23dd56cb90169ad2a241eb2cd9378ad28eea2225
Milan Miletic <milan007miletic@gmail.com>
2021-11-29 13:23:27 +0000

    IMDB scraper script

--- scraper.py
+++ scraper.py
@@ -1,12 +1,9 @@
 import csv
-
 from bs4 import BeautifulSoup
 import requests
-import re
-from scrapy.http import TextResponse
-from urllib.parse import urljoin
 import random
 
+# Collect English (US) movies from the database
 movie_codes = []
 with open("data.tsv", "rt", encoding="utf8") as f:
     for line in f:
@@ -16,16 +13,24 @@ with open("data.tsv", "rt", encoding="utf8") as f:
 
 
 def random_list(list, num_elements, seed=5):
+    """
+    Function to pick random elements from a list
+    :param list: input list
+    :param num_elements: number of elements to pick randomly
+    :param seed: integer; used for reproducible results
+    :return: random subset of input list with num_elements elements
+    """
     random.seed(seed)
     return random.choices(list, k=num_elements)
 
 
+# Scraping IMDB movie reviews and ratings
+# for random choice of 5000 movies from our list
 reviews, ratings = [], []
 for code in random_list(movie_codes, 5000, 3):
     url = "https://www.imdb.com/title/{}/reviews/?ref_=tt_ql_urv".format(code)
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    # print(code)
     data = soup.findAll('div', class_='lister-item-content')
     if data:
         for item in data:
@@ -37,29 +42,12 @@ for code in random_list(movie_codes, 5000, 3):
                 rating_label = rating.find('span')
                 ratings.append(rating_label.text)
 
-                # print(review.text)
-                # print("-------------------------------")
-                # print(rating.text)
-            # print("================================")
-            # print("================================")
-
-
 print(ratings[0])
 print(reviews[0])
 
+# Save the reviews and ratings into a .tsv file
 with open("movie_reviews2.tsv", "wt", encoding="utf8") as out_f:
     tsv_writer = csv.writer(out_f, delimiter="\t")
     for rating, review in zip(ratings, reviews):
         tsv_writer.writerow([rating, review])
-
-
-# url = 'http://www.imdb.com/chart/top'
-# response = requests.get(url)
-# soup = BeautifulSoup(response.text, 'lxml')
-#
-# movies = soup.select('td.titleColumn')
-# # links = [a.attrs.get('href') for a in soup.select('td.titleColumn a')]
-# # crew = [a.attrs.get('title') for a in soup.select('td.titleColumn a')]
-# ratings = [b.attrs.get('data-value') for b in soup.select('td.posterColumn span[name=ir]')]
-# votes = [b.attrs.get('data-value') for b in soup.select('td.ratingColumn strong')]
 
